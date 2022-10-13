9f4dfd97ed8fcc5ac914b385198b5599af22da9b
Dorian <info@dorianm.com>
2022-10-13 23:44:16 +0200

    header to be wrapped in specific hoc

---
+++ src/components/TableHeader.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import { PageHeader, Button } from "antd"
+import ModalForm from "./ModalForm"
+
+const TableHeader = ({ title, subTitle, actionScope, actionId }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  return (
+    <>
+    <PageHeader
+      onBack={() => console.log("back")}
+      title={title}
+      subTitle={subTitle}
+      extra={
+        <Button type="primary" onClick={openModal}>
+          Add {actionScope}
+        </Button>
+      }
+    />
+    <ModalForm
+        open={isModalOpen}
+        modalCloseHandler={closeModal}
+        actionType="add"
+        actionScope={actionScope}
+      />
+    </>
+  )
+}
+
+export default TableHeader
