e066de1a816017dd50c8545671d6c5d3e58bb966
Dorian <info@dorianm.com>
2022-10-17 11:46:04 +0200

    reusable (add, edit, in, out) form with selectors

--- src/components/ModalForm.js
+++ src/components/ModalForm.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 import {
   Modal,
   Form,
@@ -8,14 +9,11 @@ import {
   DatePicker,
   Tag
 } from "antd"
+import { selectInbound } from "../features/inbounds/inboundsSlice"
+import { getAllItems } from "../store/itemsSlice"
+import moment from "moment"
 import "./ModalForm.css"
-
-import mockdata from "../mockdata"
-
-const options = mockdata.items.map((el) => {
-  return { value: el.name }
-})
-console.log(options)
+import { selectOutbound } from "../features/outbounds/outboundsSlice"
 
 const ModalForm = ({
   open,
@@ -24,23 +22,57 @@ const ModalForm = ({
   actionScope,
   actionId
 }) => {
-  console.log(`modal form rendered with id ${actionId}`)
+  const editedItemData = useSelector((state) => {
+    if (actionScope === "inbound") {
+      return selectInbound(state, actionId)
+    } else if (actionScope === "outbound") {
+      return selectOutbound(state, actionId)
+    } else return null
+  })
+  const allItems = useSelector(getAllItems)
+
+  const [isNewItem, setIsNewItem] = useState(false)
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    if (open) form.resetFields()
+  }, [open, form, actionId])
+
+  const options = allItems.map((el) => {
+    return { value: el.name }
+  })
+
   const itemLabel = () => {
     return (
       <div className="Form-item--label">
         <span>Item</span>
-        <Tag color="blue">New item type</Tag>
+        {isNewItem ? <Tag color="blue">New item type</Tag> : ""}
       </div>
     )
   }
 
+  const initialValues = {
+    itemName: editedItemData ? editedItemData.itemName : "",
+    price: editedItemData ? editedItemData.price : "",
+    units: editedItemData ? editedItemData.units : "",
+    date: editedItemData ? moment(editedItemData.date, "YYYY-MM-DD") : ""
+  }
+
   const handleSubmit = () => {
-    console.log(`submitting a ${actionType}-${actionScope} form for id ${actionId}`)
-    modalCloseHandler()
+    // preparation:
+    // if there is an edited item data check if current values have changed
+    // if not then just close the modal and notify that no data is changed
+    // if yes, check if theres a new item
+    form
+      .validateFields()
+      .then((values) => {
+        console.log(values)
+        modalCloseHandler()
+      })
+      .catch((err) => console.warn(err))
   }
 
   const handleCancel = () => {
-    console.log("cancel")
     modalCloseHandler()
   }
 
@@ -49,6 +81,18 @@ const ModalForm = ({
       actionType[0].toUpperCase() + actionType.slice(1) + " " + actionScope
     if (actionType === "edit") title += ` #${actionId}`
     return <strong>{title}</strong>
+  }
+
+  const handleFieldChange = (field) => {
+    if (
+      field.name[0] === "itemName" &&
+      field.touched &&
+      !allItems.find((item) => item.name === field.value)
+    ) {
+      setIsNewItem(true)
+    } else {
+      setIsNewItem(false)
+    }
   }
 
   return (
@@ -60,15 +104,21 @@ const ModalForm = ({
       title={formTitle()}
     >
       <Form
-        className="Form-grid"
+        form={form}
+        className={`Form-grid Form-grid--${actionScope}`}
         layout="vertical"
         colon={false}
         requiredMark={false}
+        initialValues={initialValues}
+        preserve={false}
+        onFieldsChange={(field, _) => {
+          handleFieldChange(field[0])
+        }}
       >
         <Form.Item
           className="Form-item"
           label={itemLabel()}
-          name="item"
+          name="itemName"
           rules={[
             {
               required: true,
@@ -99,19 +149,23 @@ const ModalForm = ({
         >
           <InputNumber min={1} className="Form-input" />
         </Form.Item>
-        <Form.Item
-          className="Form-price"
-          label="Price per unit"
-          name="price"
-          rules={[
-            {
-              required: true,
-              message: "Please input price per unit!"
-            }
-          ]}
-        >
-          <InputNumber min={0.01} addonAfter="PLN" className="Form-input" />
-        </Form.Item>
+        {actionScope === "inbound" ? (
+          <Form.Item
+            className="Form-price"
+            label="Price per unit"
+            name="price"
+            rules={[
+              {
+                required: true,
+                message: "Please input price per unit!"
+              }
+            ]}
+          >
+            <InputNumber min={0.01} addonAfter="PLN" className="Form-input" />
+          </Form.Item>
+        ) : (
+          ""
+        )}
         <Form.Item
           className="Form-date"
           label="Date"
@@ -119,7 +173,7 @@ const ModalForm = ({
           rules={[
             {
               required: true,
-              message: "Please input inbound/outbound date!" // TODO dynamic in/out message
+              message: "Please input a valid date!"
             }
           ]}
         >
