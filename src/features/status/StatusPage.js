8fa42c080c320f532646f12b09ee0ebddb16347c
Dorian <dorian.maczka@gmail.com>
2022-10-23 22:30:04 +0200

    total stock value fix

--- src/features/status/StatusPage.js
+++ src/features/status/StatusPage.js
@@ -1,23 +1,28 @@
 import React from "react"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { statusSelector, totalValueSelector } from "./statusSelectors"
+import { statusSelector } from "./statusSelectors"
 
 import { PageHeader } from "antd"
 import StatusTable from "./StatusTable"
 
 const StatusPage = () => {
   const tableData = useSelector(statusSelector)
-  const totalValue = useSelector(totalValueSelector)
+  const totalValue = tableData.reduce((previous, current) => {
+    return previous + current.price * (current.inbound - current.outbound)
+  }, 0)
 
   const navigate = useNavigate()
-  const navigateHome = () => navigate('/')
+  const navigateHome = () => navigate("/")
 
   return (
     <>
       <PageHeader
         title="Storage status"
-        subTitle={`${totalValue.toFixed(2)} PLN`}
+        subTitle={`Estimated stock value: ${totalValue.toLocaleString("pl-PL", {
+          style: "currency",
+          currency: "PLN"
+        })}`}
         onBack={navigateHome}
       />
       <StatusTable tableData={tableData} />
