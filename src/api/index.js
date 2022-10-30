04810fbaeafbde62df9027693cd31c4d32a75022
Dorian <dorian.maczka@gmail.com>
2022-10-30 20:16:40 +0100

    edit helper

--- src/api/index.js
+++ src/api/index.js
@@ -48,6 +48,25 @@ const api = {
     }
   },
 
+  async edit(url, data) {
+    const options = {
+      // i really dont want to configure preflights on my dev php server so post it is
+      method: process.env.NODE_ENV === 'production' ? 'PUT' : 'POST',
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "text/plain" // it looks like my cors settings dont like app/json
+      },
+      body: JSON.stringify(data)
+    }
+    try {
+      const response = await fetch(url, options)
+      if (response.ok) return response.json()
+      if (!response.ok) throw new Error(response.status)
+    } catch (err) {
+      throw err
+    }
+  },
+
   // timeout for throwing errors to be caught by worker sagas
   async timeout(time = 5000) {
     await new Promise(res => setTimeout(() => res(), time))
