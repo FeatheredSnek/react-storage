fd44e63fb638fdf9eda454fb41db87aa5483721c
Dorian <dorian.maczka@gmail.com>
2022-10-31 16:10:11 +0100

    TheComponent naming for singletons

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ import OverviewPage from "./features/overview/OverviewPage"
 import StatusPage from "./features/status/StatusPage"
 import ErrorPage from "./components/ErrorPage"
 import AddDestination from "./features/destinations/AddDestination"
-import Notificator from "./components/Notificator"
+import TheNotificator from "./components/TheNotificator"
 
 const { Content, Sider } = Layout
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Notificator/>
+      <TheNotificator/>
       <Layout>
         <Sider
           className="Sider Sider--sticky"
